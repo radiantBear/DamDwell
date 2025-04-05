47ca22e5ea650753a68b507f72093ecedec7cdee
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-04-05 14:35:10 -0700

    Add staging area for scraped data

--- scripts/init.sql
+++ scripts/init.sql
@@ -13,6 +13,17 @@ CREATE TABLE IF NOT EXISTS listing (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS scraped_listing (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    month_rent DECIMAL(7, 2) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    campus_walk_time TIME NOT NULL,
+    campus_bike_time TIME NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS feature (
     id INT PRIMARY KEY AUTO_INCREMENT,
     feature VARCHAR(255) NOT NULL,
@@ -21,6 +32,14 @@ CREATE TABLE IF NOT EXISTS feature (
 );
 
 CREATE TABLE IF NOT EXISTS listing_feature (
+    listing_id INT NOT NULL,
+    feature_id INT NOT NULL,
+    PRIMARY KEY (listing_id, feature_id),
+    FOREIGN KEY (listing_id) REFERENCES listing(id) ON DELETE CASCADE,
+    FOREIGN KEY (feature_id) REFERENCES feature(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS scraped_listing_feature (
     listing_id INT NOT NULL,
     feature_id INT NOT NULL,
     PRIMARY KEY (listing_id, feature_id),
