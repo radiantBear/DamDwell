5479e686ebae3802c6bbae4fc8383eb24fe843bd
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-04-05 14:28:48 -0700

    Add bare-bones database setup

---
+++ scripts/init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS damdwell;
+
+USE damdwell;
+
+CREATE TABLE IF NOT EXISTS listing (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    month_rent DECIMAL(7, 2) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    campus_walk_time TIME NOT NULL,
+    campus_bike_time TIME NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS feature (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    feature VARCHAR(255) NOT NULL,
+    color VARCHAR(8) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS listing_feature (
+    listing_id INT NOT NULL,
+    feature_id INT NOT NULL,
+    PRIMARY KEY (listing_id, feature_id),
+    FOREIGN KEY (listing_id) REFERENCES listing(id) ON DELETE CASCADE,
+    FOREIGN KEY (feature_id) REFERENCES feature(id) ON DELETE CASCADE
+);
