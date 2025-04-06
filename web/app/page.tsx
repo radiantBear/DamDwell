aa96582a664fcaa2c8ba6a71e303ce30d21fcae9
Steven Sarber <sesarber@gmail.com>
2025-04-05 18:30:22 -0700

    added map

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Filters from "./components/filters";
 import RentalCard from "./components/rentalCard";
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
 
 export default function Home() {
   return (
@@ -8,7 +10,18 @@ export default function Home() {
 
       <div className="w-full h-800 bg-amber-300 flex">
         <div className="w-1/2 h-150 bg-amber-400">
-          
+          <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string} onLoad={() => console.log('Maps API has loaded.')}>
+            <Map
+              defaultZoom={13}
+              defaultCenter={{ lat: 44.578994, lng: -123.274673 }}
+              onCameraChanged={ (ev: MapCameraChangedEvent) =>
+                console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+              }
+              >
+            {/* <PoiMarkers pois={locations} /> */}
+            </Map>
+            <h1>Hello</h1>
+          </APIProvider>
         </div>
 
         <div className="w-1/2 h-150 bg-amber-800 p-2 flex flex-wrap gap-2">
