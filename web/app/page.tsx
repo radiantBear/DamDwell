d4bf55e9f38357cc978e965aa01abad1ac3c7e76
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-04-05 23:16:29 -0700

    Integrate map with database

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,45 +1,15 @@
-'use client';
-import React, { useState, useEffect } from 'react';
 import { Box } from "@mui/material";
 import Filters from "./components/filters";
 import RentalCard from "./components/rentalCard";
-import {Map, MapCameraChangedEvent, Marker} from '@vis.gl/react-google-maps';
+import Map from "./components/map";
+import { getListings } from "@/lib/database/listing";
 
 
-const addresses = [
-  {
-    street: "1141+NW+26th+Street",
-    city: "Corvallis",
-    state: "OR",
-    zip: "97330"
-  },
-  {
-    street: "2305 NW Monroe Ave",
-    city: "Corvallis",
-    state: "OR",
-    zip: "97330"
+export default async function Home() {
+  const listings = await getListings();
+  if (!listings) {
+    return <div>Error loading listings</div>;
   }
-]
-
-export default function Home() {
-  const [markers, setMarkers] = useState<{position: {lat: number, lng: number}}[]>([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      addresses.forEach(async (address) => {
-        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.street},+${address.city},+${address.state},${address.zip}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`)
-        const data = await response.json()
-        console.log(data)
-        setMarkers(prevMarkers => [...prevMarkers, {
-          position: {
-            lat: data.results[0].geometry.location.lat,
-            lng: data.results[0].geometry.location.lng
-          }
-        }])
-      })
-    }
-    fetchData()
-  }, [])
 
   return (
     <div>
@@ -47,30 +17,23 @@ export default function Home() {
 
       <div className="w-full h-800 flex">
         <div className="flex-grow h-150">
-          <Map
-            defaultZoom={13}
-            defaultCenter={{ lat: 44.578994, lng: -123.274673 }}
-            onCameraChanged={ (ev: MapCameraChangedEvent) =>
-              console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-            }
-            >
-
-              {markers.map((marker) => (
-                <Marker
-                  position={marker.position}
-                />
-              ))}
-          </Map>
+          <Map address_streets={listings.map(listing => listing.address)} />
         </div>
 
         <Box width="33%" maxWidth={345} gap={2} margin={2} display="flex" flexDirection="column" >
-          <RentalCard />
-          <RentalCard />
-          <RentalCard />
-          <RentalCard />
-          <RentalCard />
-          <RentalCard />
-          <RentalCard />
+          {listings.map((listing) => (
+            <RentalCard
+              key={listing.id}
+              address={listing.address}
+              rent={listing.month_rent}
+              availableDate={listing.availability_start.toLocaleDateString('en-US', {
+                month: 'short',
+                day: '2-digit',
+                year: 'numeric'
+              })}
+              image=''
+            />
+          ))}
         </Box>
       </div>
     </div>
